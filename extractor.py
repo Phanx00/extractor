8d7e8bc6d28887dd27f0706402c9712beaa6f60a
Angelo Passaro <14933081+angelopassaro@users.noreply.github.com>
2024-10-02 12:25:01 +0200

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -29,7 +29,7 @@ exclude_file = [
 ]
 
 #TODO choise for user agent
-user_agent = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0"
+user_agent = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0"}
 
 # Global variables to hold results and cookies
 results = {}
